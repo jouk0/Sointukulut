950aaa310268936993388070b31312db3cc7cebd
Mika Petteri Korhonen <mika.petteri.korhonen@gmail.com>
2022-11-28 11:24:25 +0200

    Lisäsin omat power soinnut sivut.

--- src/app/etusivu/etusivu.component.ts
+++ src/app/etusivu/etusivu.component.ts
@@ -34,55 +34,70 @@ export class EtusivuComponent implements OnInit {
   },{
     nimi: 'Omat riffit',
     sivut: '10'
+  },{
+    nimi: 'Voima sointu riffit',
+    sivut: '11-14'
   }]
   public chords: Array<any> = [{
     name: 'C',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'C#',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'D',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'D#',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'E',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'F',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'F#',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'G',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'G#',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'A',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'A#',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   },{
     name: 'H',
     duurit: [],
-    mollit: []
+    mollit: [],
+    color: ''
   }]
   public mollit: Array<any> = [{
     name: 'Cm'
@@ -109,6 +124,301 @@ export class EtusivuComponent implements OnInit {
     name: 'G#'
   },{
     name: 'A#'
+  }]
+  public powerChordRiffit: Array<any> = [{
+    name: 'C',
+    chords: [{
+      name: 'C',
+      class: 'cee'
+    },{
+      name: 'D',
+      class: 'dee'
+    },{
+      name: 'D#',
+      class: 'deeCharp'
+    },{
+      name: 'F',
+      class: 'eff'
+    },{
+      name: 'G',
+      class: 'gee'
+    },{
+      name: 'A',
+      class: 'aue'
+    },{
+      name: 'A#',
+      class: 'aueSharp'
+    }]
+  },{
+    name: 'C#',
+    chords: [{
+      name: 'C#',
+      class: 'ceeCharp'
+    },{
+      name: 'D#',
+      class: 'deeCharp'
+    },{
+      name: 'E',
+      class: 'eee'
+    },{
+      name: 'F#',
+      class: 'effSharp'
+    },{
+      name: 'G#',
+      class: 'geeSharp'
+    },{
+      name: 'A#',
+      class: 'aueSharp'
+    },{
+      name: 'H',
+      class: 'eich'
+    }]
+  },{
+    name: 'D',
+    chords: [{
+      name: 'D',
+      class: 'dee'
+    },{
+      name: 'E',
+      class: 'eee'
+    },{
+      name: 'F',
+      class: 'eff'
+    },{
+      name: 'G',
+      class: 'gee'
+    },{
+      name: 'A',
+      class: 'aue'
+    },{
+      name: 'A',
+      class: 'aue'
+    },{
+      name: 'H',
+      class: 'eich'
+    },{
+      name: 'C',
+      class: 'cee'
+    }]
+  },{
+    name: 'D#',
+    chords: [{
+      name: 'D#',
+      class: 'deeCharp'
+    },{
+      name: 'F',
+      class: 'eff'
+    },{
+      name: 'F#',
+      class: 'effSharp'
+    },{
+      name: 'G#',
+      class: 'geeSharp'
+    },{
+      name: 'A#',
+      class: 'aueSharp'
+    },{
+      name: 'G',
+      class: 'gee'
+    },{
+      name: 'G#',
+      class: 'geeSharp'
+    }]
+  },{
+    name: 'E',
+    chords: [{
+      name: 'E',
+      class: 'eee'
+    },{
+      name: 'F#',
+      class: 'effSharp'
+    },{
+      name: 'G',
+      class: 'gee'
+    },{
+      name: 'A',
+      class: 'aue'
+    },{
+      name: 'H',
+      class: 'eich'
+    },{
+      name: 'C#',
+      class: 'ceeCharp'
+    },{
+      name: 'D',
+      class: 'dee'
+    }]
+  },{
+    name: 'F',
+    chords: [{
+      name: 'F',
+      class: 'eff'
+    },{
+      name: 'G',
+      class: 'gee'
+    },{
+      name: 'G#',
+      class: 'geeSharp'
+    },{
+      name: 'A#',
+      class: 'aueSharp'
+    },{
+      name: 'C#',
+      class: 'ceeCharp'
+    },{
+      name: 'D',
+      class: 'dee'
+    },{
+      name: 'D#',
+      class: 'deeCharp'
+    }]
+  },{
+    name: 'F#',
+    chords: [{
+      name: 'F#',
+      class: 'effSharp'
+    },{
+      name: 'G#',
+      class: 'geeSharp'
+    },{
+      name: 'A',
+      class: 'aue'
+    },{
+      name: 'H',
+      class: 'eich'
+    },{
+      name: 'C#',
+      class: 'ceeCharp'
+    },{
+      name: 'D',
+      class: 'dee'
+    },{
+      name: 'E',
+      class: 'eee'
+    }]
+  },{
+    name: 'G',
+    chords: [{
+      name: 'G',
+      class: 'gee'
+    },{
+      name: 'A',
+      class: 'aue'
+    },{
+      name: 'A#',
+      class: 'aueSharp'
+    },{
+      name: 'C',
+      class: 'cee'
+    },{
+      name: 'D',
+      class: 'dee'
+    },{
+      name: 'D',
+      class: 'dee'
+    },{
+      name: 'E',
+      class: 'eee'
+    },{
+      name: 'F#',
+      class: 'effSharp'
+    }]
+  },{
+    name: 'G#',
+    chords: [{
+      name: 'G#',
+      class: 'geeSharp'
+    },{
+      name: 'A#',
+      class: 'aueSharp'
+    },{
+      name: 'H',
+      class: 'eich'
+    },{
+      name: 'C#',
+      class: 'ceeCharp'
+    },{
+      name: 'D#',
+      class: 'deeCharp'
+    },{
+      name: 'F',
+      class: 'eff'
+    },{
+      name: 'F#',
+      class: 'effSharp'
+    }]
+  },{
+    name: 'A',
+    chords: [{
+      name: 'A',
+      class: 'aue'
+    },{
+      name: 'H',
+      class: 'eich'
+    },{
+      name: 'C',
+      class: 'cee'
+    },{
+      name: 'D',
+      class: 'dee'
+    },{
+      name: 'E',
+      class: 'eee'
+    },{
+      name: 'F#',
+      class: 'effSharp'
+    },{
+      name: 'G',
+      class: 'gee'
+    }]
+  },{
+    name: 'A#',
+    chords: [{
+      name: 'A#',
+      class: 'aueSharp'
+    },{
+      name: 'C',
+      class: 'cee'
+    },{
+      name: 'C#',
+      class: 'ceeCharp'
+    },{
+      name: 'D#',
+      class: 'deeCharp'
+    },{
+      name: 'F',
+      class: 'eff'
+    },{
+      name: 'F#',
+      class: 'effSharp'
+    },{
+      name: 'G#',
+      class: 'geeSharp'
+    }]
+  },{
+    name: 'H',
+    chords: [{
+      name: 'H',
+      class: 'eich'
+    },{
+      name: 'C#',
+      class: 'ceeCharp'
+    },{
+      name: 'D',
+      class: 'dee'
+    },{
+      name: 'E',
+      class: 'eee'
+    },{
+      name: 'F#',
+      class: 'effSharp'
+    },{
+      name: 'G',
+      class: 'gee'
+    },{
+      name: 'A',
+      class: 'aue'
+    }]
   }]
   public omatRiffit: Array<any> = [{
     name: 'D',
